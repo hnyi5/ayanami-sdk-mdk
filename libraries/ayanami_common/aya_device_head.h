ef340abac0c30bcfd916dcf897ba21c5f4becf84
kurehatian <1316497021@qq.com>
2023-07-26 17:40:10 +0800

    mdk lib first commit

---
+++ libraries/ayanami_common/aya_device_head.h
@@ -0,0 +1,27 @@
+/*
+ * aya_common_driver_head.h
+ *
+ *  Created on: 2023Äê6ÔÂ2ÈÕ
+ *      Author: 13164
+ */
+
+#ifndef AYA_COMMON_DRIVER_HEAD_H_
+#define AYA_COMMON_DRIVER_HEAD_H_
+
+#include "string.h"
+#include "stdlib.h"
+#include "stdio.h"
+
+#include "utils/uartstdio.h"
+
+#include "isr.h"
+#include "bsxlite_interface.h"
+#include "aya_driver_gpio.h"
+#include "aya_driver_i2c.h"
+#include "aya_driver_timer.h"
+#include "aya_driver_spi.h"
+
+#include "aya_device_ips130.h"
+#include "aya_device_bmi088.h"
+#include "aya_device_infrared.h"
+#endif
